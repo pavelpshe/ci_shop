2ac9732566d545f1e4df1bcc1d7f765d03d08e84
Paul Pshenichny <paul.pshenichny@gmail.com>
2015-03-20 18:58:15 +0200

    Refactored user controller and user model

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -1,72 +1,103 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Model_user extends CI_Model{
-  public function user_validate($email, $pwd){
-    $pwd=make_hash($pwd);
-    $sql="SELECT * FROM users u
-          JOIN roles r ON u.id=r.uid
-          WHERE u.email=? AND u.password = ? LIMIT 1";
-    $query=$this->db->query($sql,array($email,$pwd));
-    if($query->num_rows()>0){
-      $user=$query->row_array();
-      $set_user['uid'] = $user['id'];
-      $set_user['name'] = $user['name'];
-      $set_user['email'] = $user['email'];
-      $set_user['role'] = $user['role'];
-      
-      return $this->set_login($set_user);
-    }
-     return false; 
-  }
-  public function functionName($value='')
-  {
-      
-  }
-   public function user_save($post){
-    
-    $pwd = make_hash($post['password']);
-    
-    $sql = "INSERT INTO users VALUES('', ?, ?, ?)";
-    $this->db->query($sql, array($post['name'], $post['email'], $pwd));
-    
-    if($this->db->affected_rows()){
-      
-      $id = $this->db->insert_id();
-      
-      $data = array( 'uid' => $id, 'role' => 2);//role=2(not admin static)
+/**
+ * Class Model_user
+ */
+class Model_user extends CI_Model
+{
+    /**
+     * Check user and password combination
+     * Make login when them both exists and correct
+     *
+     * @param $email
+     * @param $pwd
+     * @return bool
+     */
+    public function userValidate($email, $pwd)
+    {
+        $pwd = make_hash($pwd);
+        $sql = "SELECT *
+                FROM users u
+                  JOIN roles r ON u.id = r.uid
+                WHERE u.email=? AND u.password = ? LIMIT 1";
 
-      $this->db->insert('roles', $data); 
-      
-      $set_user['uid'] = $id;
-      $set_user['role'] = 2;
-      $set_user['name'] = $post['name'];
-      $set_user['email'] = $post['email'];
-      
-      $this->set_login($set_user);
- 
-      return true;
-      
+        $query = $this->db->query($sql, array($email, $pwd));
+
+        //something wrong, return false
+        if ($query->num_rows() != 0) {
+            return false;
+        }
+
+        $user = $query->row_array();
+        $set_user['uid'] = $user['id'];
+        $set_user['name'] = $user['name'];
+        $set_user['email'] = $user['email'];
+        $set_user['role'] = $user['role'];
+
+        return $this->setLogin($set_user);
+
     }
-    
-    return false;
-    
-  }
-   public function email_exist($email){
-    
-    $sql="SELECT email FROM users
-          WHERE email=? ";
-    $query=$this->db->query($sql,array($email));
-    if($query->num_rows()>0){
-      
-      
-      return true;
+
+    /**
+     * Add new user into the DB
+     * Make login as well
+     *
+     * @param $post
+     * @return bool
+     */
+    public function userSave($post)
+    {
+        $pwd = make_hash($post['password']);
+
+        $sql = "INSERT INTO users VALUES(NULL, ?, ?, ?)";
+        $this->db->query($sql, array($post['name'], $post['email'], $pwd));
+
+        if (!$this->db->affected_rows()) {
+            return false;
+        }
+
+        $id = $this->db->insert_id();
+        $data = array('uid' => $id, 'role' => 2);//role=2(not admin static)
+
+        $this->db->insert('roles', $data);
+
+        $set_user['uid'] = $id;
+        $set_user['role'] = 2;
+        $set_user['name'] = $post['name'];
+        $set_user['email'] = $post['email'];
+
+        $this->setLogin($set_user);
+
+        return true;
+
     }
-     return false; 
-  }
-  public function set_login($user){
-      $user['admin']=($user['role'] == 1) ? true : false;
-      unset($user['role']);
-      $this->session->set_userdata($user);
-      return true;
-  }
+
+    /**
+     * Check for Email in DB
+     * @param $email
+     * @return bool
+     */
+    public function emailExist($email)
+    {
+        $sql = "SELECT email FROM users WHERE email = ? ";
+        $query = $this->db->query($sql, array($email));
+        if ($query->num_rows() > 0) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Make login, with provided data
+     *
+     * @param $user
+     * @return bool
+     */
+    public function setLogin($user)
+    {
+        $user['admin'] = ($user['role'] == 1) ? true : false;
+        unset($user['role']);
+        $this->session->set_userdata($user);
+        return true;
+    }
 }
