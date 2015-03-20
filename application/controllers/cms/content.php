1084971e0df8961cd4e8a5f7fbc249c895c37a94
Paul Pshenichny <paul.pshenichny@gmail.com>
2015-03-20 19:07:39 +0200

    Beautify files

--- application/controllers/cms/content.php
+++ application/controllers/cms/content.php
@@ -1,35 +1,45 @@
 <?php
 if (!defined('BASEPATH'))
-  exit('No direct script access allowed');
+    exit('No direct script access allowed');
 
-class Content extends MY_Controller {
-  public $post;
-  function __construct() {
-    parent::__construct();
-    
-    if (!$this -> data['is_admin']) {
-      redirect(site_url() . 'user/login/');
+class Content extends MY_Controller
+{
+    public $post;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->data['is_admin']) {
+            redirect(site_url() . 'user/login/');
+        }
+        $this->load->library('form_validation');
+        $this->load->model('model_cms');
+        //zagrujaem model_cms
     }
-    $this -> load -> library('form_validation');
-    $this -> load -> model('model_cms');
-    //zagrujaem model_cms
-  } 
- public function index()
- {
-     echo 'Content';
- }
- 
- public function content()
- {
-     
- }public function addContent()
- {
-     
- }public function editContent()
- {
-     
- }public function deleteContent()
- {
-     
- }
+
+    public function index()
+    {
+        echo 'Content';
+    }
+
+    public function content()
+    {
+
+    }
+
+    public function addContent()
+    {
+
+    }
+
+    public function editContent()
+    {
+
+    }
+
+    public function deleteContent()
+    {
+
+    }
 }
--- application/controllers/cms/menu.php
+++ application/controllers/cms/menu.php
@@ -1,67 +1,68 @@
 <?php
 if (!defined('BASEPATH'))
-  exit('No direct script access allowed');
+    exit('No direct script access allowed');
 
-class Menu extends MY_Controller {
-  public $post;
-  function __construct() {
-    parent::__construct();
-    
-    if (!$this -> data['is_admin']) {
-      redirect(site_url() . 'user/login/');
-    }
-    $this -> load -> library('form_validation');
-    $this -> load -> model('model_cms');
-    //zagrujaem model_cms
-  } 
- public function index()
- {
-     $menu=$this->model_cms->getMenu();
-     if($menu){
-       $this->data['content']=$this->parser->parse('cms/menu',$menu,true);
-     }   
-     $this->load->view('cms/main',$this->data);
- }
- public function addMenu()
- {
-    
-   
-    $this->post=$this->input->post(null,true);  
-    $this->form_validation->set_rules('title','Title','trim|required|xss_clean');
-    $this->form_validation->set_rules('link','Link','trim|required|xss_clean');
-    
-    if ($this->form_validation->run()==false){
-     
-      $this->data['content']= $this->load->view('cms/add_menu_form',$this->data,true);
-      $this->load->view('cms/main',$this->data);
-     }
-     else{
-        $this->model_cms->saveMenu($this->post);
-      redirect(site_url().'cms/menu/');
-    }
-    
- }
- 
- public function editMenu($id=null)
- {
-     
- }
- public function deleteMenu($id=null)
- {
- 
-     
-$this->post=$this->input->post(null,true);  
-    $this->form_validation->set_rules('title','Title','trim|required|xss_clean');
-    $this->form_validation->set_rules('link','Link','trim|required|xss_clean');
-    
-    
-      $this->data['id']=$id;
-      $this->data['content']= $this->load->view('cms/delete_menu_form',$this->data,true);
-      $this->load->view('cms/main',$this->data);
-     }
-     else{
-        $this->model_cms->saveMenu($this->post);
-      redirect(site_url().'cms/menu/');
+class Menu extends MY_Controller
+{
+    public $post;
+
+    function __construct()
+    {
+        parent::__construct();
+
+        if (!$this->data['is_admin']) {
+            redirect(site_url() . 'user/login/');
+        }
+        $this->load->library('form_validation');
+        $this->load->model('model_cms');
+        //zagrujaem model_cms
     }
 
+    public function index()
+    {
+        $menu = $this->model_cms->getMenu();
+        if ($menu) {
+            $this->data['content'] = $this->parser->parse('cms/menu', $menu, true);
+        }
+        $this->load->view('cms/main', $this->data);
+    }
+
+    public function addMenu()
+    {
+        $this->post = $this->input->post(null, true);
+        $this->form_validation->set_rules('title', 'Title', 'trim|required|xss_clean');
+        $this->form_validation->set_rules('link', 'Link', 'trim|required|xss_clean');
+
+        if ($this->form_validation->run() == false) {
+
+            $this->data['content'] = $this->load->view('cms/add_menu_form', $this->data, true);
+            $this->load->view('cms/main', $this->data);
+        } else {
+            $this->model_cms->saveMenu($this->post);
+            redirect(site_url() . 'cms/menu/');
+        }
+
+    }
+
+    public function editMenu($id = null)
+    {
+
+    }
+
+    public function deleteMenu($id = null)
+    {
+        if ($id) {
+            $this->post = $this->input->post(null, true);
+            $this->form_validation->set_rules('title', 'Title', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('link', 'Link', 'trim|required|xss_clean');
+
+            $this->data['id'] = $id;
+            $this->data['content'] = $this->load->view('cms/delete_menu_form', $this->data, true);
+            $this->load->view('cms/main', $this->data);
+        } else {
+            $this->model_cms->saveMenu($this->post);
+            redirect(site_url() . 'cms/menu/');
+        }
+
+    }
 }
