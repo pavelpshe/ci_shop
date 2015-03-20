fc44bc7a79bb93ad2cb3480943b11c0afb8ba89f
Paul Pshenichny <paul.pshenichny@gmail.com>
2015-03-20 13:59:49 +0200

    Refactired products controller

--- application/controllers/products.php
+++ application/controllers/products.php
@@ -1,44 +1,62 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-class Products extends MY_Controller {
-  function __construct(){
-    parent::__construct();
-    $this->load->model('model_products');
-    
-  }
+/**
+ * Class Products
+ */
+class Products extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('model_products');
 
-	public function index($category=null,$product = null) {
-	  
-    if(is_null($category)){//pokazivaet vse kategorii
-	  $this->data['title']='Our categories';
-    $cat=$this->model_products->getCategories();
-    if($cat){
-      $this->data['content']=$this->parser->parse('parser/categories',$cat,TRUE);
-    } 
     }
-    
-    elseif(!is_null($category) && is_null($product)){//pokazat chto v categorii
-     
-     $categories=$this->security->xss_clean($category);//vstroennaya funkciya v codeigniter dlya xss_clean
-     
-     $prd=$this->model_products->getProducts($category);
-      if ($prd){
-        $this->data['title']=$prd['cat_name'].'Products';
-        $this->data['content']=$this->parser->parse('parser/products',$prd,TRUE);
-       
-     }
-    }elseif(!is_null($category) && !is_null($product)){//pokazat chto v categorii
-     
-     $product=$this->security->xss_clean($product);
-     $item=$this->model_products->getItem($product);
-     if ($item){
-        $this->data['title']=$item['title'];
-        $this->data['content']=$this->parser->parse('parser/item',$item,TRUE);
-       
-     }
+
+    /**
+     * @param null $category
+     * @param null $product
+     */
+    public function index($category = null, $product = null)
+    {
+
+        //Filter variable, check for injections
+        $category = $this->security->xss_clean($category);
+        $product = $this->security->xss_clean($product);
+
+        //Show all categories, no category selected
+        if (is_null($category)) {
+            $this->data['title'] = 'Our categories';
+            $cat = $this->model_products->getCategories();
+            if ($cat) {
+                $this->data['content'] = $this->parser->parse('parser/categories', $cat, TRUE);
+            }
+            return $this->_finishIt();
+        }
+
+        //Show one category
+        if (is_null($product)) {
+            $prd = $this->model_products->getProducts($category);
+            if ($prd) {
+                $this->data['title'] = $prd['cat_name'] . 'Products';
+                $this->data['content'] = $this->parser->parse('parser/products', $prd, TRUE);
+
+            }
+            return $this->_finishIt();
+        }
+
+        $item = $this->model_products->getItem($product);
+        if ($item) {
+            $this->data['title'] = $item['title'];
+            $this->data['content'] = $this->parser->parse('parser/item', $item, TRUE);
+
+        }
+
+        return $this->_finishIt();
     }
-    $this->load->view('templates/main', $this->data);
-		
-	}
-  
+
+    protected function _finishIt()
+    {
+        return $this->load->view('templates/main', $this->data);
+    }
+
 }
